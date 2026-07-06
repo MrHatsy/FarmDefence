97be231eb5a0af5edf427242ca1d56ca9a42400c
Lara <lara.ergun@students.mq.edu.au>
2026-07-06 16:02:11 +1000

    Update Attack Quicker

--- FarmDefenceGame/Assets/Scripts/Attack.cs
+++ FarmDefenceGame/Assets/Scripts/Attack.cs
@@ -5,6 +5,7 @@ public class Attack : MonoBehaviour
     [SerializeField] private Transform attack;
     [SerializeField] private Vector3 targetScale = new Vector3(2f, 2f, 2f);
     private float scaleSpeed = 0.03f;
+    private Vector3 maxScale = new Vector3(5f, 5f, 5f);
     private PlayerMovement Player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,10 +23,16 @@ public class Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        AttackRadius();
+    }
+
+    private void AttackRadius()
+    {
         if (!Player.haunted)
         {
             return;
         }
-        transform.localScale = transform.localScale + new Vector3(0.002f, 0.002f, 0.002f);
+        Vector3 nextScale = transform.localScale + new Vector3(0.01f, 0.01f, 0.01f);
+        transform.localScale = Vector3.Min(nextScale, maxScale);
     }
 }
