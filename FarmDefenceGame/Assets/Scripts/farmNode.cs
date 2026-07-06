a3122ae62d9f6031b9c017968186ee4c6035719f
Matthew <125848106+MrHatsy@users.noreply.github.com>
2026-07-06 13:39:30 +1000

    farms should be able to be formed into a path

--- FarmDefenceGame/Assets/Scripts/farmNode.cs
+++ FarmDefenceGame/Assets/Scripts/farmNode.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
 
-public class farmNode : MonoBehaviour
+public class FarmNode : MonoBehaviour
 {
+    [SerializeField] private int hp;
+    [SerializeField] private bool alive;
+    [SerializeField] private FarmNode nextNode;
+    private object myPointManager;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        myPointManager = FindFirstObjectByType(PointManager);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (alive == true)
+        {
+            //gain points
+            myPointManager.addPoints();
+
+            //take damage
+
+        }
+    }
+
+    FarmNode getNextNode()
+    {
+        return nextNode;
     }
 }
--- FarmDefenceGame/Assets/Scripts/pointManager.cs
+++ FarmDefenceGame/Assets/Scripts/pointManager.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
-public class pointManager : MonoBehaviour
+public class PointManager : MonoBehaviour
 {
+    private int points;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        points = 0;
     }
 
     // Update is called once per frame
@@ -14,7 +16,7 @@ public class pointManager : MonoBehaviour
         
     }
 
-    void addPoints(){
-        
+    public void addPoints(){
+        points += 10;
     }
 }
